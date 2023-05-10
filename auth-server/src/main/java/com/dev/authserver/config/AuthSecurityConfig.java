a1f98f1aeafea764fa52b38098f2bd31e95a9ebd
Beatriz <beeatrizz29@gmail.com>
2023-05-10 15:06:30 -0300

    Auth Server

---
+++ auth-server/src/main/java/com/dev/authserver/config/AuthSecurityConfig.java
@@ -0,0 +1,117 @@
+package com.dev.authserver.config;
+
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.time.Duration;
+import java.util.Arrays;
+
+@Configuration
+public class AuthSecurityConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        return http.formLogin(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+
+//        RegisteredClient authTestClient = RegisteredClient
+//                .withId("1")
+//                .clientId("authTest")
+//                .clientSecret(passwordEncoder.encode("123456"))
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+//                .scope("read")
+//                .scope("write")
+//                .scope("trust")
+//                .tokenSettings(TokenSettings.builder()
+//                        .accessTokenTimeToLive(Duration.ofMinutes(5))
+//                        .build())
+//                .clientSettings(ClientSettings.builder()
+//                        .requireAuthorizationConsent(false)
+//                        .build())
+//                .build();
+
+        RegisteredClient authTestClient = RegisteredClient
+                .withId("1")
+                .clientId("authTest")
+                .clientSecret(passwordEncoder.encode("123456"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://localhost:3000/authorized")
+                .redirectUri("https://oidcdebugger.com/debug")
+                .scope("read")
+                .scope("write")
+                .scope("trust")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofMinutes(15))
+                        .refreshTokenTimeToLive(Duration.ofDays(1))
+                        .reuseRefreshTokens(false)
+                        .build())
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true)
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(Arrays.asList(authTestClient));
+    }
+
+    @Bean
+    public AuthorizationServerSettings providerSettings(AuthProperties authProperties) {
+        return AuthorizationServerSettings.builder()
+                .issuer(authProperties.getProviderUri())
+                .build();
+    }
+
+    @Bean
+    public JWKSet jwkSet(AuthProperties authProperties) throws Exception {
+        final var jksProperties = authProperties.getJks();
+        final String jksPath = jksProperties.getPath();
+        final InputStream inputStream = new ClassPathResource(jksPath).getInputStream();
+
+        final KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(inputStream, jksProperties.getStorepass().toCharArray());
+
+        RSAKey rsaKey = RSAKey.load(keyStore,
+                jksProperties.getAlias(),
+                jksProperties.getKeypass().toCharArray());
+
+        return new JWKSet(rsaKey);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource (JWKSet jwkSet) {
+        return (((jwkSelector, context) -> jwkSelector.select(jwkSet)));
+    }
+
+
+}
