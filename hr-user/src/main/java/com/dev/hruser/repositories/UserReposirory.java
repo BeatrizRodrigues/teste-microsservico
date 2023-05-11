d920880031ef5b2fe3a6a1a885b09a55edc7a5ab
Beatriz <beeatrizz29@gmail.com>
2023-05-11 16:31:35 -0300

    findByUsuario

--- hr-user/src/main/java/com/dev/hruser/repositories/UserReposirory.java
+++ hr-user/src/main/java/com/dev/hruser/repositories/UserReposirory.java
@@ -1,2 +1,10 @@
-package com.dev.hruser.repositories;public class UserReposirory {
+package com.dev.hruser.repositories;
+
+import com.dev.hruser.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReposirory extends JpaRepository<User, Long> {
+
+    User findByUsuario(String usuario);
+
 }
--- hr-user/src/main/java/com/dev/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/dev/hruser/resources/UserResource.java
@@ -1,2 +1,28 @@
-package com.dev.hruser.resources;public class UserResource {
+package com.dev.hruser.resources;
+
+import com.dev.hruser.domain.User;
+import com.dev.hruser.repositories.UserReposirory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserReposirory repository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByUsuario(@RequestParam String usuario) {
+        User obj = repository.findByUsuario(usuario);
+        return ResponseEntity.ok(obj);
+    }
+
 }
